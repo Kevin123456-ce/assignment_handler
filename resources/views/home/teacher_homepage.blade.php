6192a93a30c8f4f3f74782de8bf927c64c962359
Ronak Padaliya <ronakpadaliya77@gmail.com>
2021-03-18 19:56:39 +0530

    Small error fix

---
+++ resources/views/home/teacher_homepage.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<title>Home Page</title>
+<head>Teacher Home Page</head>
+<body>
+    <br><a href="create_class">Click</a> here to create new class<br>
+    <a href="view_class">Click</a> here to view all class<br>
+</body>
+</html>
